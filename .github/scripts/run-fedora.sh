649a4d5a9c8755516903863f90a7f2d656300679
rmartinc <rmartinc@redhat.com>
2023-02-06 16:59:47 +0100

    test2

---
+++ .github/scripts/run-fedora.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+dnf install -y java-11-openjdk-devel nss-tools crypto-policies-scripts maven-openjdk11
+fips-mode-setup --enable --no-bootcfg
+fips-mode-setup --check
+mvn -B -fae clean install
