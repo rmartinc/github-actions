9ff235a5423d230acfef1038313cd119682313b0
rmartinc <rmartinc@redhat.com>
2023-02-08 18:59:27 +0100

    test

--- .github/scripts/run-fedora.sh
+++ .github/scripts/run-fedora.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-find /repository
+find /github/home
 dnf install -y java-11-openjdk-devel nss-tools crypto-policies-scripts maven-openjdk11
 fips-mode-setup --enable --no-bootcfg
 fips-mode-setup --check
-JAVA_HOME=/etc/alternatives/java_sdk_11 mvn test -B -fae -pl testsuite
+./mvnw test -B -fae -pl testsuite
