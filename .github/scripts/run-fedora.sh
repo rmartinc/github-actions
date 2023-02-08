fa0638c33ecb611aa3ec5396680e4ecec75066d3
rmartinc <rmartinc@redhat.com>
2023-02-08 09:29:18 +0100

    lala

--- .github/scripts/run-fedora.sh
+++ .github/scripts/run-fedora.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-pwd
-ls -la
 dnf install -y java-11-openjdk-devel nss-tools crypto-policies-scripts maven-openjdk11
 fips-mode-setup --enable --no-bootcfg
 fips-mode-setup --check
-mvn -B -fae clean install
+JAVA_HOME=/etc/alternatives/java_sdk_11 mvn test -B -fae -pl testsuite
