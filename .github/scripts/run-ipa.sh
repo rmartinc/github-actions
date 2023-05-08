3b1ebee31b1fa21e23e0640cd679645cd89efcb2
rmartinc <rmartinc@redhat.com>
2023-05-08 13:01:28 +0200

    lala

--- .github/scripts/run-ipa.sh
+++ .github/scripts/run-ipa.sh
@@ -1,53 +1,20 @@
 #!/bin/bash
 
-id -a
-pwd
-cat /etc/hosts
+# start the container for free-ipa
+container=$(podman run --detach --rm -h ipa.example.test --sysctl net.ipv6.conf.all.disable_ipv6=0 --workdir /github/workspace -v "/home/rmartinc/NetBeansProjects/github-actions":"/github/workspace" -v "$HOME/.m2":"/root/.m2" freeipa/freeipa-server:fedora-rawhide ipa-server-install --unattended --realm=EXAMPLE.TEST --ds-password=password --admin-password=password --idstart=60000)
 
-ipa-server-install --unattended --realm=EXAMPLE.TEST --ds-password=password --admin-password=password --idstart=60000
+# loop until the ipa server is started
+sleep 30
+line=$(podman logs $container | tail -1)
+regexp="FreeIPA server configured."
+while ! [[ "$line" =~ "$regexp" ]]; do
+  sleep 30
+  line=$(podman logs $container | tail -1)
+done
 
-if ! grep -q ^ldap_user_extra_attrs /etc/sssd/sssd.conf; then
-  sed -i '/ldap_tls_cacert/a ldap_user_extra_attrs = mail:mail, sn:sn, givenname:givenname, telephoneNumber:telephoneNumber' /etc/sssd/sssd.conf
-fi
-if ! grep -q ^user_attributes /etc/sssd/sssd.conf; then
-  sed -i '/allowed_uids/a user_attributes = +mail, +telephoneNumber, +givenname, +sn' /etc/sssd/sssd.conf
-fi
+podman exec $container .github/scripts/run-ipa-tests.sh
+result=$?
 
-systemctl restart sssd
-sss_cache -E
+podman stop $container
 
-echo "auth    required   pam_sss.so" >>/etc/pam.d/keycloak
-echo "account required   pam_sss.so" >>/etc/pam.d/keycloak
-
-printf "%b" "password\n" | kinit admin
-ipa group-add --desc='test group' testgroup
-ipa user-add emily --first=Emily --last=Jones --email=emily@jones.com --random
-ipa group-add-member testgroup --users=emily
-ipa user-add bart --first=bart --last=bart --email= --random
-ipa user-add david --first=david --last=david --random
-kdestroy
-
-ldapmodify -D "cn=Directory Manager" -w password <<EOF
-dn: uid=emily,cn=users,cn=accounts,dc=example,dc=test
-changetype: modify
-replace: userpassword
-userpassword: emili123
-
-dn: uid=bart,cn=users,cn=accounts,dc=example,dc=test
-changetype: modify
-replace: userpassword
-userpassword: bart123
-
-dn: uid=david,cn=users,cn=accounts,dc=example,dc=test
-changetype: modify
-replace: userpassword
-userpassword: david123
-
-EOF
-
-printf "%b" "password\n" | kinit admin
-ipa user-disable david
-kdestroy
-
-./mvnw clean install
-
+exit $result
