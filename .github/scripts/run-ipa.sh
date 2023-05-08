6eedfa6addd7e50608354980f07e46837dcb74d0
rmartinc <rmartinc@redhat.com>
2023-05-08 13:05:35 +0200

    lala

--- .github/scripts/run-ipa.sh
+++ .github/scripts/run-ipa.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # start the container for free-ipa
-container=$(podman run --detach --rm -h ipa.example.test --sysctl net.ipv6.conf.all.disable_ipv6=0 --workdir /github/workspace -v "/home/rmartinc/NetBeansProjects/github-actions":"/github/workspace" -v "$HOME/.m2":"/root/.m2" freeipa/freeipa-server:fedora-rawhide ipa-server-install --unattended --realm=EXAMPLE.TEST --ds-password=password --admin-password=password --idstart=60000)
+container=$(podman run --detach --rm -h ipa.example.test --sysctl net.ipv6.conf.all.disable_ipv6=0 --workdir /github/workspace -v "$JAVA_HOME":$JAVA_HOME" -v "$1":"/github/workspace" -v "$HOME/.m2":"/root/.m2" freeipa/freeipa-server:fedora-rawhide ipa-server-install --unattended --realm=EXAMPLE.TEST --ds-password=password --admin-password=password --idstart=60000)
 
 # loop until the ipa server is started
 sleep 30
